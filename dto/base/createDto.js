805d90f2062d23441ce30b8f4f8f6cc1f574c814
Emmanuel <caronnunu@gmail.com>
2023-02-21 12:16:38 +0100

    create dto for create project

---
+++ dto/base/createDto.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+const schemaValidator = require('../../middleware/shemaValidatorMiddleware');
+
+//shema de de verification des donner en entré de la route
+module.exports = (req, res, next) => {
+    const joiCreateProject = Joi.object({
+        name : Joi.string().required().min(3).max(50),
+        date_start: Joi.date().iso(),
+        date_end : Joi.date().iso(),
+        description : Joi.string().required().max(255),
+    });
+    schemaValidator(req, joiCreateProject, next);
+}
