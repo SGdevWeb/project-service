eb0829329c0dc0582b597640e9b1513eb482947f
Emmanuel <caronnunu@gmail.com>
2023-02-21 11:27:49 +0100

    Create Model Project

---
+++ model/projectModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+//schama de la data des collections
+const projectSchema = mongoose.Schema({
+    id: Number,
+    uuid: String,
+    name: String,
+    date_start: { type: Date },
+    date_end: { type: Date },
+    description: String
+});
+
+//nom de la collection  = nom du model+s exemple nom du model : project, nom de la collections : projects
+module.exports = mongoose.model("project", projectSchema);
