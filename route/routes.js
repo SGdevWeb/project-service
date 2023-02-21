03ad865ed1f692e38e6b0992cb4942666f00ed09
Emmanuel <caronnunu@gmail.com>
2023-02-21 15:02:21 +0100

    simplify routes

--- route/routes.js
+++ route/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const DTO = require('../dto/dtos');
+const Controller = require("../controller/controller");
 
-const projectRoutes = require("./projectRoute");
-
-router.use(projectRoutes);
+router.post('/create', DTO.project.create, Controller.project.create);
 
 module.exports = router;
