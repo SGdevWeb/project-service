5f92e8049dabc047df611bdcd4f83287c3134b64
Emmanuel <caronnunu@gmail.com>
2023-02-21 15:36:46 +0100

    Split routes type and project

---
+++ route/projectRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const DTO = require('../dto/dtos');
+const Controller = require("../controller/controller");
+
+router.post('/create', DTO.project.create, Controller.project.create);
+
+module.exports = router;
--- route/routes.js
+++ route/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const DTO = require('../dto/dtos');
-const Controller = require("../controller/controller");
 
-router.post('/create', DTO.project.create, Controller.project.create);
+const projectRoutes = require("./projectRoutes");
+const typeRoutes = require("./typeRoutes");
+
+router.use("/project", projectRoutes);
+router.use("/type", typeRoutes);
 
 module.exports = router;
