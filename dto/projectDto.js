970f7a18fde20fc71e2ef32be98a5cf252fbfd54
Emmanuel <caronnunu@gmail.com>
2023-03-13 15:02:52 +0100

    Fix dto description create project

--- dto/projectDto.js
+++ dto/projectDto.js
@@ -7,7 +7,7 @@ const create = (req, res, next) => {
     name: Joi.string().required().min(3).max(50),
     date_start: Joi.date().required().iso(),
     date_end: Joi.date().iso(),
-    description: Joi.string().required().max(255),
+    description: Joi.string().required().max(5000),
     uuid_user: Joi.string().guid({ version: "uuidv4" }).required(),
     //type : Joi.string().required().length(32),
   });
